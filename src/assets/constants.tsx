ba4fa6d72054aed82094c87617a4fee408d6d0ee
moong <moonki0623@naver.com>
2023-06-06 12:48:58 +0900

    [chore]: project structure setup

---
+++ src/assets/constants.tsx
@@ -0,0 +1,19 @@
+const Z_INDEX_SIDEBAR = 100;
+const SIDEBAR_WIDTH = "20rem";
+
+const nameRegEx = /^[가-힣]+$/;
+const idRegEx = /^[a-zA-Z0-9_]{4,16}$/;
+
+// 적어도 하나의 소문자, 대문자, 숫자, 특수문자 포함, 최소 8자 이상 ex. Aabb3ccd!ss
+const pwRegEx =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&`])[A-Za-z\d@$!%*?&`]{8,}$/;
+const emailRegEx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+export {
+  SIDEBAR_WIDTH,
+  Z_INDEX_SIDEBAR,
+  nameRegEx,
+  idRegEx,
+  pwRegEx,
+  emailRegEx,
+};
