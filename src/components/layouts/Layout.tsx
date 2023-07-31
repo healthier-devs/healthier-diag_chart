617c20d941f4bfe2660fa95660c35085b6929ce8
moonki <moonki0623@naver.com>
2023-08-01 04:06:30 +0900

    feat: TS with layout settings

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -1,8 +1,8 @@
-import Sidebar from "@/components/sidebar";
+import Sidebar from "@/components/Sidebar";
 import { ReactNode } from "react";
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="w-[100vw] h-[100vh] flex flex-row">
+    <div className="w-[100vw] h-[100vh] flex flex-row p-4 bg-app_gray_100">
       <Sidebar />
       {children}
     </div>
