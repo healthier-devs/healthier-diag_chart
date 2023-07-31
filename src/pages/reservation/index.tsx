fe02defc58592f3bf445d4cd7929a4995e23ce17
moonki <moonki0623@naver.com>
2023-08-01 04:07:30 +0900

    remove: remove unsed file

--- src/pages/reservation/index.tsx
+++ src/pages/reservation/index.tsx
@@ -4,19 +4,23 @@ import Layout from "@/components/layouts/Layout";
 
 import TimeTable from "@/components/timetable";
 
-const ReservationPage: NextPageWithLayout = () => {
-  return (
-    <div className={`w-Content h-[100vh]`}>
-      {/* [TEMP]: items and justify for temporary, change needed */}
-      <div className="w-full h-full relative">
-        <TimeTable />
-      </div>
-    </div>
-  );
-};
+// const ReservationPage: NextPageWithLayout = () => {
+//   return (
+//     <div className={`w-Content h-[100vh]`}>
+//       {/* [TEMP]: items and justify for temporary, change needed */}
+//       <div className="relative w-full h-full">
+//         <TimeTable />
+//       </div>
+//     </div>
+//   );
+// };
 
-ReservationPage.getLayout = function getLayout(page: ReactElement) {
-  return <Layout>{page}</Layout>;
+// ReservationPage.getLayout = function getLayout(page: ReactElement) {
+//   return <Layout>{page}</Layout>;
+// };
+
+const ReservationPage = () => {
+  return <>WIP</>;
 };
 
 export default ReservationPage;
