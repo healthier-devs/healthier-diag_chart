6759d08b794ece52fd521d8052fd7edc7678a7c4
moonki <moonki0623@naver.com>
2023-08-01 04:07:52 +0900

    feat: login logic updated

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,51 +1,86 @@
 import Image from "next/image";
-import { useForm } from "react-hook-form";
-import { ILoginFormInput } from "@/utils/types";
+import appLogoSrc from "@/assets/images/appLogo.png";
+import { useState } from "react";
+import { onLogin } from "@/api/auth";
+import { setCookie } from "@/utils/cookies";
+import { useRouter } from "next/router";
 
 const Login = () => {
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-  } = useForm<ILoginFormInput>({ mode: "onBlur" });
+  const Router = useRouter();
+  const [loginData, setLoginData] = useState({
+    id: "",
+    pw: "",
+  });
+
+  const handleLogin = () => {
+    if (loginData.id === "" || loginData.pw === "") {
+      alert("아이디와 비밀번호를 입력해주세요");
+    } else {
+      onLogin({
+        username: loginData.id,
+        password: loginData.pw,
+      })
+        .then((res) => {
+          console.log(res);
+          if (res.accessToken === undefined) {
+            throw new Error("아이디와 비밀번호를 확인해주세요");
+          } else {
+            localStorage.setItem(
+              "refreshToken",
+              JSON.stringify(res.refreshToken)
+            );
+            localStorage.setItem("accessToken", res.accessToken); // cookie저장으로 바꾸기
+            Router.push("/patientList");
+          }
+        })
+        .catch((err) => {
+          alert("아이디와 비밀번호를 확인해주세요");
+        });
+    }
+  };
   return (
     <div className="w-[100vw] h-[100vh] bg-app_gray_900 flex justify-center items-center">
       <div className="w-[37.5%] h-full flex flex-col justify-center items-center mb-10">
         <div className="w-[43.5%] h-9 relative mb-10">
           <Image
-            src="/images/appLogo.png"
+            src={appLogoSrc}
             alt="appLogo"
             fill
             sizes={"100%"}
             style={{ objectFit: "contain" }}
           />
         </div>
-        <div className="w-full flex flex-col justify-start items-center p-10 bg-app_gray_850 rounded-3xl">
-          <div className="w-full flex flex-col gap-2 text-lg mb-6">
-            <div className="text-font_gray_400 self-start">ID</div>
+        <div className="flex flex-col items-center justify-start w-full p-10 bg-app_gray_850 rounded-3xl">
+          <div className="flex flex-col w-full gap-2 mb-6 text-lg">
+            <div className="self-start text-font_gray_400">ID</div>
             <input
               className="w-full h-14 rounded-[80px] py-4 px-8 text-sm"
-              {...register("id", {
-                required: "아이디를 입력해주세요",
-              })}
               placeholder="아이디를 입력해주세요"
+              value={loginData.id}
+              onChange={(e) => {
+                setLoginData({ ...loginData, id: e.target.value });
+              }}
             />
           </div>
-          <div className="w-full flex flex-col gap-2 mb-8">
-            <div className="text-font_gray_400 self-start">PW</div>
+          <div className="flex flex-col w-full gap-2 mb-8">
+            <div className="self-start text-font_gray_400">PW</div>
             <input
               type="password"
               className="w-full h-14 rounded-[80px] py-4 px-8 text-sm"
-              {...register("pw", {
-                required: "비밀번호를 입력해주세요",
-              })}
               placeholder="비밀번호를 입력해주세요"
+              value={loginData.pw}
+              onChange={(e) => {
+                setLoginData({ ...loginData, pw: e.target.value });
+              }}
             />
           </div>
-          <button className="mt-8 w-full h-14 rounded-[80px] p-4 bg-app_healthier_blue text-white text-base">
+          <button
+            className="mt-8 w-full h-14 rounded-[80px] p-4 bg-app_healthier_blue text-white text-base"
+            onClick={handleLogin}
+          >
             로그인 하기
           </button>
-          <table className="mt-12 border-collapse w-full text-center text-font_gray_400 font-medium">
+          <table className="w-full mt-12 font-medium text-center border-collapse text-font_gray_400">
             <tbody>
               <tr>
                 <th className="border-r-2 border-gray-600">아이디 찾기</th>
