0585b71538b77e1aae05515f95d6b13db8da3071
moonki <moonki0623@naver.com>
2023-08-05 11:57:58 +0900

    feat: changed patient list handling in recoil

--- src/utils/atom.tsx
+++ src/utils/atom.tsx
@@ -1,5 +1,19 @@
 import { atom } from "recoil";
 
+interface PatientDataProps {
+  visitRecordId: string;
+  patient: {
+    name: string;
+    birthDate: string;
+    gender: string;
+    age: number;
+  };
+  submitTime: string;
+  cc: string;
+  soapUuid: string;
+  status: string;
+}
+
 export const searchWaitingPatient = atom({
   key: "searchWaitingPatient",
   default: "",
@@ -8,4 +22,9 @@ export const searchWaitingPatient = atom({
 export const searchCompletePatient = atom({
   key: "searchCompletePatient",
   default: "",
+});
+
+export const patientRecoilList = atom<PatientDataProps[]>({
+  key: "patientList",
+  default: [],
 });
