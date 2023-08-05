3b2f1142ade1252f4110c4661d5cdff051f77e4e
moonki <moonki0623@naver.com>
2023-08-05 11:56:41 +0900

    feat: getting hospital information

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,5 +1,29 @@
-import { POST } from "@/utils/axios";
+import { GET, POST } from "@/utils/axios";
 
 export const onLogin = async (body: any) => {
-  return await POST(`/signin`, body);
+  const data = await POST(`/signin`, body);
+
+  return data;
+};
+
+export const onReissue = async () => {
+  POST(`/reissue`, {
+    refreshToken: localStorage.getItem("refreshToken"),
+    accessToken: localStorage.getItem("accessToken"),
+  })
+    .then((res) => {
+      localStorage.setItem("accessToken", res.data.accessToken);
+      localStorage.setItem("refreshToken", res.data.refreshToken);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+export const getHospitalInfo = async () => {
+  return await GET("/info", {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+    },
+  });
 };
